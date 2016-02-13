5fb539332aca385a07cc179457e10b0f9af924db
ykawasak <akiu.bullet@gmail.com>
2016-02-14 00:08:36 +0900

    socket.io追加

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-serialport = require('serialport');
+var serialport = require('serialport');
 var redis = require('redis');
+var path = require('path');
 var express = require('express');
 var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-var path = require('path');
+var http = require("http").createServer(app);
+var io = require('socket.io')(http);
+http.listen(8080, "localhost");
 
 var serialport = "";
 var serialport_n = 9600;
 var subscriber = redis.createClient(6379, 'tk2-244-31758.vs.sakura.ne.jp');
 var client = redis.createClient(6379, 'tk2-244-31758.vs.sakura.ne.jp');
 var publisher = redis.createClient(6379, 'tk2-244-31758.vs.sakura.ne.jp');
+var slppe = require('sleep');
 
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
+  
 
   //var subscriber = redis.createClient();
   subscriber.subscribe('bnNotify');
@@ -33,14 +35,21 @@ app.listen(3000, function () {
 });
 
 io.on('connection', function(socket){
-    console.log('a user connected');
-    socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+  console.log('a user connected');
+
+  sleep(1000, function(){
+    io.emit("bonnou", "bonnou");
+    console.log("send");
   });
+      
 });
+//テスト用
+function sleep(time, callback){
+  setTimeout(callback, time);
+}
 
 //終了をサーバに通知
 function finish(id){
-    client.lpush("bnDelete", id);
-    publisher.publish("bnComplete", id);
+  client.lpush("bnDelete", id);
+  publisher.publish("bnComplete", id);
 }
